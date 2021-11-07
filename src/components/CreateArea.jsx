2d7eb1613dd6bcba972d8b71ae97275ce4d77bc3
ChidponJaimun <61172207+ChidponJaimun@users.noreply.github.com>
2021-11-08 03:56:08 +0700

    10

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
 
 function CreateArea(props) {
   const [note, setNote] = useState({
@@ -6,10 +9,12 @@ function CreateArea(props) {
     content: ""
   });
 
+  const [shouldExpand, setShouldExpand] = useState(false);
+
   function handleChange(event) {
     const { name, value } = event.target;
 
-    setNote(prevNote => {
+    setNote((prevNote) => {
       return {
         ...prevNote,
         [name]: value
@@ -26,23 +31,34 @@ function CreateArea(props) {
     event.preventDefault();
   }
 
+  function rowExpand(event) {
+    setShouldExpand(true);
+  }
+
   return (
     <div>
       <form className="create-note">
-        <input
-          name="title"
-          onChange={handleChange}
-          value={note.title}
-          placeholder="Title"
-        />
+        {shouldExpand && (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        )}
         <textarea
           name="content"
           onChange={handleChange}
+          onClick={rowExpand}
           value={note.content}
           placeholder="Take a note..."
-          rows="3"
+          rows={shouldExpand ? 3 : 1}
         />
-        <button onClick={submitNote}>Add</button>
+        <Zoom in={shouldExpand}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
